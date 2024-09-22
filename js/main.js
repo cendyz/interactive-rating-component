b81c5e02dc1573c9cbc8f26feba0d808f9529841
cendyz <arek57823@gmail.com>
2024-09-22 11:21:57 +0200

    update again again java

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const firstCard = document.querySelector(".box__first-card");
 const secondCard = document.querySelector(".box__second-card");
 const submitBtn = document.querySelector(".box__first-card-submit-btn");
-const checkBtns = document.querySelector(".box__first-card-btns");
+const checkBtns = document.querySelectorAll(".box__first-card-btn");
 const selectedNum = document.querySelector(".box__second-card-selected-text p span");
 
 let newNum = "";
@@ -11,9 +11,11 @@ const handleDisplay = () => {
 	secondCard.style.display = "grid";
 };
 
-checkBtns.addEventListener("click", e => {
-	newNum = e.target.textContent;
-	selectedNum.textContent = newNum;
+checkBtns.forEach(num => {
+	num.addEventListener("click", e => {
+		newNum = e.target.textContent;
+		selectedNum.textContent = newNum;
+	});
 });
 
 submitBtn.addEventListener("click", () => {
